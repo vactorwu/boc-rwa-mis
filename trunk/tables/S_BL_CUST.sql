e7ad54c6cf28b747cc740d0b5b6ec85789c55ba2
sean.wu.china <sean.wu.china@b621f81e-db32-11de-9872-ef835a306e00>
2009-11-30 08:39:09 +0000

    add check error columns and upload columns

--- trunk/tables/S_BL_CUST.sql
+++ trunk/tables/S_BL_CUST.sql
@@ -1,31 +1,35 @@
 ------------------------------------------------------------
---  交易对手信息
+-- 客户信息
 -- S_BL_CUST
 ------------------------------------------------------------
 DROP TABLE S_BL_CUST;
 
 -- 创建补录表单
-create table 	S_BL_CUST			(
-	DTTYPE		char(1)	,
-	SYSCDE		varchar2(10)	,
-	AREANO		varchar2(20)	,
-	CUSIDT		varchar2(50)	,
-	CUSDES		varchar2(100)	,
-	CUSTTP		varchar2(12)	,
-	INCOTRY		varchar2(50)	,
-	RATEAGY1		varchar2(20)	,
-	RATING1		varchar2(10)	,
-	RATEAGY2		varchar2(20)	,
-	RATING2		varchar2(10)	,
-	RATEAGY3		varchar2(20)	,
-	RATING3		varchar2(10)	,
-	RATEAGY4		varchar2(20)	,
-	RATING4		varchar2(10)	,
-	TOTSALE		number	,
-	TOTASST		number	,
-	CCYCODE		varchar2(3)	,
-	INDUSCDE		varchar2(20)	,
-	DEFAULTTP		char(1)	
+create table 	S_BL_CUST	(
+	DTTYPE			char(1) default 'I',
+	SYSCDE			varchar2(10),
+	AREANO			varchar2(20),
+	CUSIDT			varchar2(50),
+	CUSDES			varchar2(100),
+	CUSTTP			varchar2(12),
+	INCOTRY			varchar2(50),
+	RATEAGY1		varchar2(20),
+	RATING1			varchar2(10),
+	RATEAGY2		varchar2(20),
+	RATING2			varchar2(10),
+	RATEAGY3		varchar2(20),
+	RATING3			varchar2(10),
+	RATEAGY4		varchar2(20),
+	RATING4			varchar2(10),
+	TOTSALE			number,
+	TOTASST			number,
+	CCYCODE			varchar2(3),
+	INDUSCDE		varchar2(20),
+	DEFAULTTP		char(1),
+	APPSTATUS 	char(1) default 'F',
+	UPLOAD_FLAG char(1) default 'F',
+	UPLOAD_USER number(4),
+	UPLOAD_TIME DATE
 );				
 
 -- 删除注册信息	
@@ -34,13 +38,14 @@ DELETE FROM CD_FDW_STRUCTURE WHERE TABLE_NAME = 'PK_S_BL_CUST' AND OBJECT_TYPE =
 
 -- 创建成普通表
 EXEC PACK_FERMAT.CREATE_TABLE('S_BL_CUST','ADMIN','F','');
+
 -- 标识成客制化表
 UPDATE CD_FDW_STRUCTURE SET STANDARD_CUSTOM='C' WHERE TABLE_NAME IN ('S_BL_CUST');
--- 更新DMM
-EXEC PACK_DB_OBJECT.INITIALIZE_TABLE_COLUMNS('S_BL_CUST');
+
 
 -- 重建表为分区表
 EXEC PACK_DDL.CHANGE_TABLE_TYPE('S_BL_CUST','DATA');
+
 -- 定义Policy
 EXEC PACK_INSTALL.PARTITION_DEFINE_POLICIES('S_BL_CUST');
 EXEC PACK_DDL.RECREATE_TABLE('S_BL_CUST');
@@ -49,6 +54,13 @@ EXEC PACK_DDL.RECREATE_TABLE('S_BL_CUST');
 INSERT INTO CD_FDW_STRUCTURE (TABLE_NAME,TABLE_TYPE,HIST_TABLE_NAME,OBJECT_TYPE, STANDARD_CUSTOM, INIT_MODE, PARAMETER) 
 VALUES ('PK_S_BL_CUST', 'PRIMARY', 'S_BL_CUST', 'INDEX', 'C', 'L', 'PARTITION_KEY,SYSCDE,AREANO,CUSIDT');
 CALL PACK_DDL.REBUILD_INDEX('S_BL_CUST','PK_S_BL_CUST');
+
+-- 更新DMM
+EXEC PACK_DB_OBJECT.INITIALIZE_TABLE_COLUMNS('S_BL_CUST');
+
+-- 创建为check error table
+EXEC PACK_DDL.ADD_RECHECK_COLUMNS('S_BL_CUST');
+EXEC PACK_BATCH.CREATE_CHECK_REPORT_TABLE('S_BL_CUST');
 
 -- 更新DMM
 EXEC PACK_DB_OBJECT.INITIALIZE_TABLE_COLUMNS('S_BL_CUST');
